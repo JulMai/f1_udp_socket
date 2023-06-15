590f65c2e54d15be1e9aa4c7a45373b31f5a2568
JulMai <julian_mai@outlook.de>
2023-06-15 21:28:52 +0200

    added links to the udp spec (ea forum) and code to translate the word document to json files

---
+++ links.md
@@ -0,0 +1,6 @@
+# Links
+## F1 22 UDP Specification
+https://answers.ea.com/t5/General-Discussion/F1-22-UDP-Specification/td-p/11551274
+
+## F1 23 UDP Specification
+https://answers.ea.com/t5/General-Discussion/F1-23-UDP-Specification/td-p/12632888
---
+++ src/utils/doc/load.py
@@ -0,0 +1,99 @@
+import zipfile
+import re
+import json
+import os
+
+
+def get_str_from_doc(path: str):
+    docx = zipfile.ZipFile(path)
+    content = docx.read('word/document.xml').decode('utf-8')
+    cleaned = re.sub('<(.|n)*?>', '', content)
+    return cleaned
+
+
+def get_structs(text: str):
+    pattern = r"struct\s+\w+\s*\{[^{}]*\}"
+    matches = re.findall(pattern, text)
+    return matches
+
+
+def get_struct_name(s: str):
+    return s[len("struct "):s.find('{')]
+
+
+def get_attributes(s: str):
+    re_attr = r'\w+\s+m_.{5,25};'
+    attributes = re.findall(re_attr, s)
+    return attributes
+
+
+def get_attr_name(s: str):
+    re_attr_name = r'm_\w+(\[\d+])?'
+    name = re.search(re_attr_name, s)[0][2:]
+    return name
+
+
+def get_attr_type(s: str):
+    re_type = r'^\w+'
+    type = re.search(re_type, s)[0]
+    return type
+
+
+def struct_to_json(struct: str):
+    struct_name = get_struct_name(struct)
+    attributes = get_attributes(struct)
+    dic = {}
+    for attribute in attributes:
+        name = get_attr_name(attribute)
+        type = get_attr_type(attribute)
+        dic[name] = type
+    return struct_name, dic
+
+
+def type_is_array(type: str) -> int:
+    match = re.search(r'\[(\d+)\]', type)
+    if match:
+        number = int(match.group(1))
+        return number
+    else:
+        return 0
+
+
+def structs_to_json_rec(text: str):
+    struct_strs = get_structs(text)
+    structs = {}
+    for struct in struct_strs:
+        struct_name = get_struct_name(struct)
+        if "CarDamage" in struct_name:
+            print()
+        attributes = get_attributes(struct)
+        attr = {}
+        for attribute in attributes:
+            name = get_attr_name(attribute)
+            type = get_attr_type(attribute)
+            iterator = type_is_array(name)
+            if type in structs:
+                type = structs[type]
+            if iterator > 0:
+                name = name[:name.find("[")]
+                attr[name] = [type for i in range(iterator)]
+                print()
+            else:
+                attr[name] = type
+        structs[struct_name] = attr
+    return structs
+
+
+def write_to_json(struct: dict, name: str, path: str = os.path.join(".", "packets")):
+    file_path = os.path.join(path, name) + ".json"
+    os.makedirs(path, exist_ok=True)
+    with open(file_path, "w+") as f:
+        json.dump(struct, f, indent=4)
+
+
+if __name__ == '__main__':
+    path = ""
+    text = get_str_from_doc(path)
+    structs = structs_to_json_rec(text)
+    for name, struct in structs.items():
+        write_to_json(struct, name, os.path.join(".", "data", "F123"))
